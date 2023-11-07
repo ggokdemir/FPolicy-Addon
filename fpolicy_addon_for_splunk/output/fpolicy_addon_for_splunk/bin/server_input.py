e6f3c37b5dab2f1c88d0cd33b16edd4cab1d5356
ggokdemir <ggokdemir@splunk.com>
2023-11-07 16:59:21 +0100

    Local tests are done

--- fpolicy_addon_for_splunk/output/fpolicy_addon_for_splunk/bin/server_input.py
+++ fpolicy_addon_for_splunk/output/fpolicy_addon_for_splunk/bin/server_input.py
@@ -69,20 +69,20 @@ class ModInputSERVER_INPUT(base_mi.BaseModInput):
         sock.bind((host, port))
         sock.listen(1)
         # listen for one connection at a time
-        helper.log_info(f"\n ... Listening on {host}:{port} ...")
+        helper.log_info(f"\n\n ... Listening on {host}:{port} ... \n\n")
         while True:
             # wait for a connection
             client_sock, client_addr = sock.accept()
-            helper.log_info(f"\n !! Connection from {client_addr} !!")
+            helper.log_info(f"\n\n !! Connection from {client_addr} !! \n\n")
             # receive text data
             raw_data = client_sock.recv(1024)
-            helper.log_info(f"\n **Received raw data: {raw_data} \n ")
+            helper.log_info(f"\n\n **Received raw data: {raw_data} \n\n")
             #cut the non decode part, then decode
             hex_data = raw_data[6:-1]
             unk_hex_data = raw_data[:6]
-            #helper.log_info(f"\n **Received hex data: {hex_data}")
+            #helper.log_info(f"\n\n **Received hex data: {hex_data} \n\n")
             data = hex_data.decode()
-            helper.log_info(f"\n **Received data decoded: {data} \n ")
+            helper.log_info(f"\n\n **Received data decoded: {data} \n\n")
             # here edit find the <SessionId>
             tag_start = "<SessionId>"
             tag_end = "</SessionId>"
@@ -95,44 +95,39 @@ class ModInputSERVER_INPUT(base_mi.BaseModInput):
             match_VsUUID = re.search(pattern, data)
             if (match_VsUUID and match_SessionId):
                 result_SessionId = match_SessionId.group(1)
-                helper.log_info("\n >>> SessionId : {}".format(result_SessionId))
+                helper.log_info("\n\n >>> SessionId : {}".format(result_SessionId))
                 result_VsUUID = match_VsUUID.group(1)
-                helper.log_info("\n >>> VsUUID : {}".format(result_VsUUID))
+                helper.log_info("\n\n >>> VsUUID : {}".format(result_VsUUID))
                 header_resp = ("<?xml version=\"1.0\"?><Header><NotfType>NEGO_RESP</NotfType><ContentLen>234</ContentLen><DataFormat>XML</DataFormat></Header>")
                 # send a header
-                helper.log_info("\n --> Header to send : {}".format(header_resp))
+                helper.log_info("\n\n --> Header to send : {}".format(header_resp))
                 # SessionId and VsUUID should change only
                 handshake_resp = ("<?xml version=\"1.0\"?><HandshakeResp><VsUUID>" + ("%s" % (result_VsUUID)) + "</VsUUID><PolicyName>policy-test-flo</PolicyName><SessionId>"+("%s" % (result_SessionId))+"</SessionId><ProtVersion>1.2</ProtVersion></HandshakeResp>")
-                helper.log_info(" --> Handshake response length below: _ ")
+                helper.log_info("\n\n --> Handshake response length below: _ \n")
                 helper.log_info(len(handshake_resp.encode()))
                 try:
                     # send a response
-                    helper.log_info("\n --> Response to send : {}".format(handshake_resp))
+                    helper.log_info("\n\n --> Response to send : {}".format(handshake_resp))
                     # client_sock.send(header_resp.encode()+bytes.fromhex('0a 0a')+handshake_resp.encode())
                     client_sock.send(("""\"\x00\x00\x01\x68\""""+header_resp+"\n\n"+handshake_resp).encode())
                     complete = ("""\"\x00\x00\x01\x68\""""+header_resp+"\n\n"+handshake_resp).encode()
-                    helper.log_info("!!! Complete the segment sent below : _ ")
+                    helper.log_info("\n\n !!! Complete the segment sent below : _ \n")
                     helper.log_info((complete))
                 except IOError as err:
-                    helper.log_info('\n IO Err.' + str(err))
+                    helper.log_info('\n\n IO Err.' + str(err))
             else:
-                #helper.log_info("\n SessionId and VsUUID not found.^^ Check the data above ^^")
+                #helper.log_info("\n\n SessionId and VsUUID not found.^^ Check the data above ^^")
                 #TODO: An event came, write that to an Index.
 
                 data = hex_data.decode()
-                helper.log_info(f"\n ===> Convert to JSON: \n {data} \n")
+                helper.log_info(f"\n\n ===> Data to write: \n {data} \n\n")
 
 
                 #FIXME:
                 # insert input values into the url and/or header (helper class handles credential store)
-                index=helper.get_arg('account')['index']
 
-                helper.log_info("index : "+index)
-
-                index=helper.get_arg('account')['index']
-                account=helper.get_arg('account')['index']
-
-                helper.log_info("index : "+index+"account : "+account)
+                index=helper.get_arg("index")
+                account=helper.get_arg("account")['name']
 
                 import xml.etree.ElementTree as ET
                 import json
@@ -158,24 +153,24 @@ class ModInputSERVER_INPUT(base_mi.BaseModInput):
 
                     # Convert the Python dictionary to JSON
                     json_data = json.dumps(xml_dict, indent=4)
-                    helper.log_info(f"\n ===> Converted to JSON: \n {json_data} \n")
+                    #helper.log_info(f"\n\n ===> Converted to JSON: \n {json_data} \n\n")
 
                     try:
                         sourcetype=  "server_input"  + "://" + helper.get_input_stanza_names()
                         event = helper.new_event(source="server_input", index=index, sourcetype=sourcetype , data=json_data)
-                        helper.log_info("\n   (.) JSON Event Inserted (.) \n source=\"server_input\", index="+index+", sourcetype="+sourcetype+" , data="+json_data)
+                        helper.log_info("\n\n   (.) JSON Event Inserted (.) \n source="+account+", index="+index+", sourcetype="+sourcetype+" , data="+json_data)
                         ew.write_event(event)
                     except:
-                        helper.log_info("\n   (!) Error inserting JSON event. (!)  ")
+                        helper.log_info("\n\n   (!) Error inserting JSON event. (!)  \n\n")
 
                 except:
                     try:
-                        sourcetype=  "server_input"  + "://" + helper.get_input_stanza_names()
-                        event = helper.new_event(source="server_input", index=index, sourcetype=sourcetype , data=data)
-                        helper.log_info("\n   (.) XML Event Inserted (.) \n source=\"server_input\", index="+index+", sourcetype="+sourcetype+" , data="+data)
+                        sourcetype=  account  + "://" + helper.get_input_stanza_names()
+                        event = helper.new_event(source=account, index=index, sourcetype=sourcetype , data=data)
+                        helper.log_info("\n\n   (.) XML Event Inserted (.) \n source="+account+", index="+index+", sourcetype="+sourcetype+" , data="+data)
                         ew.write_event(event)
                     except:
-                        helper.log_info("\n   (!) Error inserting XML event. (!)  ")
+                        helper.log_info("\n\n   (!) Error inserting XML event. (!)  \n\n")
 
                 #FIXME: 
 
@@ -184,7 +179,7 @@ class ModInputSERVER_INPUT(base_mi.BaseModInput):
                     # close the socket
                     client_sock.close()
                 except IOError as err:
-                    helper.log_info('\n IO Err.' + str(err))
+                    helper.log_info('\n\n IO Err.' + str(err))
 
 
     def get_account_fields(self):
